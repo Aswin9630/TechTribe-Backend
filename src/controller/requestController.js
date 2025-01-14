d98c2a1a4e10836b7bfc8393524de36ca1362cde
Aswin Krishna <aswinkrishna005@gmail.com>
2025-01-14 20:50:18 +0530

    bug fixing--while requesting connection, it should allow only certain condition

--- src/controller/requestController.js
+++ src/controller/requestController.js
@@ -11,14 +11,39 @@ const sendConnectionRequestController = async(req,res,next)=>{
         const toUserId = req.params.toUserId
         const status = req.params.status
 
+        //while requesting connection, it should allow only certain Status-Types
+        const allowedStatus = ["interested","ignored"];
+        if(!allowedStatus.includes(status)){
+            return next(errorHandler(400,`${status.toUpperCase()} is Invalid status type`))
+        }
+
+        const toUser = await User.findById(toUserId)
+        if(!toUser){
+            return next(errorHandler(404,"User not found"));
+        }
+
+        if(ConnectionRequest.fromUserId===toUserId){
+            return next(errorHandler(400,"Oops..!!! You cannot send request to yourself"))
+        }
+
+        const isConnectionRequestExist = await ConnectionRequest.findOne({
+            $or:[
+                {fromUserId,toUserId},
+                {fromUserId:toUserId, toUserId:fromUserId}
+            ]
+        })
+        if(isConnectionRequestExist){
+            return next(errorHandler(400,"Connection request already exist!"))
+        }      
+
         const newConnectionRequest = new ConnectionRequest({
             fromUserId,
             toUserId,
             status
         })
+        const data = await newConnectionRequest.save();
 
-        const data = await newConnectionRequest.save()
-        res.status(200).json({ success:true, message:`${user.firstName.toUpperCase()} send the Request`, connectionDetails:data })
+        res.status(200).json({ success:true, message:`${user.firstName.toUpperCase()} is ${status} in ${toUser.firstName}`, connectionDetails:data })
          
     } catch (error) {
         return next(errorHandler(400,error.message))
