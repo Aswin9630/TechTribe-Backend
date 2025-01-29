79002e3f63d56ab3223cce729ba2fee2bc8a6f7c
Aswin Krishna <aswinkrishna005@gmail.com>
2025-01-29 17:49:04 +0530

    created Payment Model and save the payment details in database

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -1,21 +1,36 @@
+const PaymentModel = require("../model/paymentModel");
+const User = require("../model/userModel");
 const errorHandler = require("../utils/errorHandler");
 const razorpayInstance = require("../utils/razorpay");
 
 const createOrderController = async(req, res, next)=>{
+    const {memebershipType} = req.body;
+    const {id} = req.user
+    const user = await User.findById(id);
     try {
         const order = await razorpayInstance.orders.create({
             amount: 50000,
             currency: "INR",
             receipt: "receipt#1",
             notes: {
-              firstName: "value3",
-              lastName: "value2",
-              membershipType:"gold"
+              firstName:user.firstName,
+              lastName: user.lastName,
+              email:user.email,
+              membershipType:memebershipType
             }
           })
-          console.log(order);
-           
-          res.json({order})
+          const payment = new PaymentModel({
+            userId:id,
+            orderId:order.id,
+            amount:order.amount,
+            currency:order.currency,
+            status:order.status,
+            notes:order.notes,
+            receipt:order.receipt
+          })     
+          const savedPaymentDetails = await payment.save()
+          const {...paymentDetails} = savedPaymentDetails._doc
+          res.status(200).json({paymentDetails}) 
           
     } catch (error) {
         console.error(error)
