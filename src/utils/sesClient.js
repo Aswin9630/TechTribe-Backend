fe0b2c5cd459f968ee65ac103de99b4ecb27dfa3
Aswin Krishna <aswinkrishna005@gmail.com>
2025-01-30 11:01:21 +0530

    ses file clean

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -1,7 +1,5 @@
 const  { SESClient } = require("@aws-sdk/client-ses");
-// Set the AWS Region.
 const REGION = "ap-south-1";
-// Create SES service object.
 const sesClient = new SESClient({ region: REGION,
     credentials:{
         accessKeyId:process.env.AWS_ACCESS_KEY,
